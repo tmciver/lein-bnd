022406588c16439445da8a7e9ff419cf10c52ad3
Tim McIver <tim@timmciver.com>
2014-10-13 12:04:15 -0400

    Added custom :release-tasks to project file.

--- project.clj
+++ project.clj
@@ -4,4 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[biz.aQute.bnd/bnd "2.2.0"]]
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]])
